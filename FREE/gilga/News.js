006bd3ac59d65249e77a51139e27eeba941857c3
jonnyjava <jonnyjava.net@gmail.com>
2017-04-01 10:31:50 +0200

    save last changes for posterity

--- FREE/gilga/News.js
+++ FREE/gilga/News.js
@@ -1,5 +1,5 @@
-
-  // SCRIVI QUI SOTTO. CAMBIA SOLO IL TESTO TRA LE ""
-
-
+
+  // SCRIVI QUI SOTTO. CAMBIA SOLO IL TESTO TRA LE ""
+
+
 document.write("Prossime news...........Prossime news..........Prossime news...........");
